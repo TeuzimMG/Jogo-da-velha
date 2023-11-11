542bcbb637d51a5096ffe57844e31498479300d4
TeuzimMG <basemagalha@gmail.com>
2023-11-11 15:51:07 -0300

    Atualização no while

--- main.py
+++ main.py
@@ -38,44 +38,50 @@ Jogo da velha
             print(f"O {marcacao} joga primeiro")
             print(f"O {marcacao2} joga em seguida")
             while partida:
-                x,y = [int(x) for x in input("Digite as cordenadas separadas com[:]: ").split(':')]
-                os.system("cls")
-                if vez_de_jogar % 2 == 0:
-                    if self.tab[x][y] == marcacao2:
-                        print(self.print_tabuleiro())
-                        print("Seu inimigo já marcou aqui !!!")
+                try:
+                    x,y = [int(x) for x in input("Digite as cordenadas separadas com[:]: ").split(':')]
+                    if x > 2 or y > 2:
+                        print("Coloque uma coordenada válida")
                         continue
-                    elif self.tab[x][y] == marcacao:
-                        print("Você já marcou aqui")
-                        continue
-                    self.tab[x][y] = marcacao
-                else:
-                    if self.tab[x][y] == marcacao:
-                        print(self.print_tabuleiro())
-                        print("Seu inimigo já marcou aqui !!!")
-                        continue
-                    elif self.tab[x][y] == marcacao2:
-                        print(self.print_tabuleiro())
-                        print("Você já marcou aqui")
-                        continue
-                    self.tab[x][y] = marcacao2
-                for x in marcacao,marcacao2:
-                    if (self.tab[0][0] == x and self.tab[1][1] == x and self.tab[2][2] == x):
-                        print(f"O {x} GANHOU !!!")
-                        partida = False
-                        jogo = False
-                    for c in range(0,3):
-                        if (self.tab[c][0] == x and self.tab[c][1] == x and self.tab[c][2] == x):
+                    os.system("cls")
+                    if vez_de_jogar % 2 == 0:
+                        if self.tab[x][y] == marcacao2:
+                            print(self.print_tabuleiro())
+                            print("Seu inimigo já marcou aqui !!!")
+                            continue
+                        elif self.tab[x][y] == marcacao:
+                            print("Você já marcou aqui")
+                            continue
+                        self.tab[x][y] = marcacao
+                    else:
+                        if self.tab[x][y] == marcacao:
+                            print(self.print_tabuleiro())
+                            print("Seu inimigo já marcou aqui !!!")
+                            continue
+                        elif self.tab[x][y] == marcacao2:
+                            print(self.print_tabuleiro())
+                            print("Você já marcou aqui")
+                            continue
+                        self.tab[x][y] = marcacao2
+                    for x in marcacao,marcacao2:
+                        if (self.tab[0][0] == x and self.tab[1][1] == x and self.tab[2][2] == x):
                             print(f"O {x} GANHOU !!!")
                             partida = False
                             jogo = False
-                        if (self.tab[0][c] == x and self.tab[1][c] == x and self.tab[2][c] == x):
-                            print(f"O {x} GANHOU !!!")
-                            partida = False
-                            jogo = False
-                if partida == True:
-                    print(self.print_tabuleiro())
-                vez_de_jogar += 1
+                        for c in range(0,3):
+                            if (self.tab[c][0] == x and self.tab[c][1] == x and self.tab[c][2] == x):
+                                print(f"O {x} GANHOU !!!")
+                                partida = False
+                                jogo = False
+                            if (self.tab[0][c] == x and self.tab[1][c] == x and self.tab[2][c] == x):
+                                print(f"O {x} GANHOU !!!")
+                                partida = False
+                                jogo = False
+                    if partida == True:
+                        print(self.print_tabuleiro())
+                    vez_de_jogar += 1
+                except:
+                    print("Um erro inesperado ocorreu")
                 
 
 
